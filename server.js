9f1f371affa0c8c40391a7d0b0260fe72dbf25be
manc1n1 <mancinij1111@gmail.com>
2023-09-06 21:28:50 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//TODO: Don't forget to configure your app to accept JSON
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+//TODO: Use the Express Router for more detailed routes
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port http://localhost:${PORT}`);
+    });
+  });
