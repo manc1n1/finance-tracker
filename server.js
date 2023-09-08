4134cebffb75700aeb99eeeb712ec206b21a12e3
manc1n1 <mancinij1111@gmail.com>
2023-09-07 21:56:16 -0400

    Update file structure

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/config');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,11 +15,11 @@ app.set('view engine', 'handlebars');
 //TODO: Use the Express Router for more detailed routes
 
 app.get('/', function (req, res) {
-    res.render('home');
+	res.render('home');
 });
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-      console.log(`App listening on port http://localhost:${PORT}`);
-    });
-  });
+	app.listen(PORT, () => {
+		console.log(`App listening on port http://localhost:${PORT}`);
+	});
+});
