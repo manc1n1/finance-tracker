a9eeaa70a26c543f5ffb96e973dee33b29df4cc1
manc1n1 <mancinij1111@gmail.com>
2023-09-14 22:04:22 -0400

    Update config.js file

--- config/config.js
+++ config/config.js
@@ -8,15 +8,8 @@ module.exports = {
 		host: '127.0.0.1',
 		dialect: 'mysql',
 	},
-	test: {
-		username: 'root',
-		password: null,
-		database: 'database_test',
-		host: '127.0.0.1',
-		dialect: 'mysql',
-	},
 	production: {
-		use_env_variable: 'JAWSDB_URL',
+		use_env_variable: process.env.JAWSDB_URL,
 		dialect: 'mysql',
 	},
 };
