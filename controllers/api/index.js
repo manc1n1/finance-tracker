21d0c83de7e192268316a0d0f00d03c9c43edbb1
tijanihind <tijani.hind200@gmail.com>
2023-09-16 08:47:36 -0400

    add login api route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -15,3 +15,29 @@ router.post("/user", async (req, res) => {
         res.status(400).json(error)
     }
 })
+
+// @post
+// /api/user/login
+// login in user
+router.post("/user/login",async (req,res) => {
+    try {
+        const userData = await User.findOne({where:{email:req.body.email}})
+
+        if(!userData){
+            res.status(404).json({message:"Incorrect email or password, please try again"})
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password)
+
+        if(!validPassword){
+            res.status(400).json({message:"Incorrect email or password, please try again"})
+        }
+
+        res.json({user:userData, message:"You are now logged"})
+
+    } catch (error) {
+        console.log(err)
+        res.status(400).json(err)
+    }
+})
