f7bc7ea59c1cd889be9c3f7eb654684024af77c4
tijanihind <tijani.hind200@gmail.com>
2023-09-18 09:28:33 -0400

    able to update, delete and get a users portfolio

--- controllers/api/portfolioRoutes.js
+++ controllers/api/portfolioRoutes.js
@@ -5,11 +5,13 @@ const Portfolio = require("../../db/models/portfolio")
 // /api/portfolio
 // create portfolio
 router.post('/', async (req, res) => {
-	const { name, user_id } = req.body;
+	const { name, net_worth } = req.body;
+    const user_id = req.session.user_id
 
 	try {
 		const portfolio = await Portfolio.create({
 			name,
+            net_worth,
 			user_id,
 		});
 		return res.json(portfolio);
@@ -32,5 +34,57 @@ router.get('/list', async (req, res) => {
 		return res.status(500).json({ error: 'Something went wrong.' });
 	}
 });
+
+//@get
+// api/portfolio/list-user
+// get all users portfolio
+router.get("/list-user", async (req, res) => {
+    try {
+        const user_id = req.session.user_id
+        const portfolios = await Portfolio.findAll({where:{user_id}})
+
+        return res.json(portfolios)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+//@delete
+// api/portfolio/:id
+// delete specific user portfolio
+router.delete("/:id", async(req, res) => {
+    try {
+        const id = req.params.id
+        const user_id = req.session.user_id
+
+        const [affectedRows] = await Portfolio.destroy({where:{id, user_id}})
+
+        if(affectedRows > 0) res.status(200).end()
+        else res.status(404).end()
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+// @update
+// api/portfolio/:id
+// update specific user portfolio
+router.put("/:id", async (req, res) => {
+    try {
+        const [affectedRows] = await Portfolio.update(req.body, {where:{
+            id:req.params.id,
+            user_id:req.session.user_id
+        }})
+
+        if(affectedRows > 0) res.status(200).end()
+        else res.status(404).end()
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
