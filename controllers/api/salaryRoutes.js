dc56012499dfa4e443b7417d593fa0d06068c6e0
tijanihind <tijani.hind200@gmail.com>
2023-09-18 09:45:18 -0400

    able to get and delete a portfolio salary

--- controllers/api/salaryRoutes.js
+++ controllers/api/salaryRoutes.js
@@ -36,4 +36,37 @@ router.put("/:id", async (req, res) => {
     }
 })
 
+//@get
+// api/salary/:portfolio_id
+// get portfolio salary
+router.get("/:portfolio_id", async (req, res) => {
+    try {
+        const portfolio_id = req.params.portfolio_id
+        const salary = await Salary.findAll({where:{portfolio_id}})
+
+        return res.json(salary)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+//@delete
+// api/salary/:id
+// delete specific salary
+router.delete("/:id", async(req, res) => {
+    try {
+        const id = req.params.id
+
+        const [affectedRows] = await Salary.destroy({where:{id}})
+
+        if(affectedRows > 0) res.status(200).end()
+        else res.status(404).end()
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
 module.exports = router
