418a47401cd8c501af0e594bd43edde682567946
manc1n1 <mancinij1111@gmail.com>
2023-09-19 15:32:05 -0400

    Fix signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,14 @@
-const first_name = document.querySelector('#firstname-signup').value.trim();
-const last_name = document.querySelector('#lastname-signup').value.trim();
-const email = document.querySelector('#email-signup').value.trim();
-const password = document.querySelector('#password-signup').value.trim();
 const passwordTextArea = document.querySelector('#password-signup');
 const characterCounter = document.getElementById('char_count');
 const maxNumOfChars = 8;
 
 const signupFormHandler = async (event) => {
 	event.preventDefault();
+
+	const first_name = document.querySelector('#firstname-signup').value.trim();
+	const last_name = document.querySelector('#lastname-signup').value.trim();
+	const email = document.querySelector('#email-signup').value.trim();
+	const password = document.querySelector('#password-signup').value.trim();
 
 	if (first_name && last_name && email && password) {
 		const response = await fetch('/api/user/signup', {
